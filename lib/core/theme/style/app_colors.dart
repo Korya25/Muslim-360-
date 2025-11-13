7124a4ff877638b62955a238bb998c3e2e35216a
Korya25 <????? ??? GitHub>
2025-11-13 23:26:28 +0200

    add native splash screen

--- lib/core/theme/style/app_colors.dart
+++ lib/core/theme/style/app_colors.dart
@@ -4,13 +4,13 @@ class AppColors {
   const AppColors._();
 
   // Nav Bar
-  static const bottomNavBarBackground = Color(0xFF2A2A2A);
-  static const bottomNavBarSelected = Color(0xFF7ED957);
+  static const bottomNavBarBackground = Color(0xFF1A1A1A);
+  static const bottomNavBarSelected = Color(0xFF35A306);
   static const bottomNavBarSelectedIcon = Colors.black;
   static const bottomNavBarUnselectedIcon = Colors.white;
   static Color bottomNavBarBorder = Colors.grey.withAlpha(51);
   static Color bottomNavBarShadow = Colors.black.withAlpha(77);
 
   // Scaffold background
-  static const scaffoldBackground = Colors.black;
+  static const scaffoldBackground = Color(0xFF0F0F0F);
 }
