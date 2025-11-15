4a42e118d723aae39abb4f501ae2594b0c107127
Korya25 <????? ??? GitHub>
2025-11-15 21:04:31 +0200

    Update dependencies and refactor PrayerView; remove unused SliverAppBar and PinnedHeaderDelegate components

--- lib/features/prayer/presentation/views/prayer_view.dart
+++ lib/features/prayer/presentation/views/prayer_view.dart
@@ -1,33 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:muslim360/core/theme/style/app_colors.dart';
-import 'package:muslim360/features/prayer/presentation/widgets/SliverAppBar/prayer_sliver_app_bar.dart';
-import 'package:muslim360/features/prayer/presentation/widgets/SliverPersistentHeader/pinned_header_delegate.dart';
 
 class PrayerView extends StatelessWidget {
   const PrayerView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: CustomScrollView(
-        slivers: [
-          PrayerSliverAppBar(),
-
-          SliverPersistentHeader(
-            pinned: true,
-            delegate: PinnedHeaderDelegate(
-              child: Container(
-                color: AppColors.bottomNavBarBackground,
-                alignment: Alignment.center,
-                child: const Text(
-                  "",
-                  style: TextStyle(color: AppColors.textWhite, fontSize: 18),
-                ),
-              ),
-            ),
-          ),
-        ],
-      ),
-    );
+    return Scaffold(body: Container());
   }
 }
